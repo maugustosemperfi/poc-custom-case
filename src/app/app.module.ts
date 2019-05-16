916d94b7d15d871dbe730792c9e5f78c908ee6c5
Marcos Augusto Vieira Martins <marcos.martins@mv.com.br>
2019-05-16 13:45:48 -0300

    [FEATURE] Adding NGXS to aplication

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,9 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CoreModule } from './core/core.module';
+import { NgxsModule } from '@ngxs/store';
+import { NgxsReduxDevtoolsPluginModule } from '@ngxs/devtools-plugin';
+import { NgxsRouterPluginModule } from '@ngxs/router-plugin';
 
 @NgModule({
   declarations: [
@@ -12,6 +15,9 @@ import { CoreModule } from './core/core.module';
   ],
   imports: [
     CoreModule,
+    NgxsModule.forRoot(),
+    NgxsReduxDevtoolsPluginModule.forRoot(),
+    NgxsRouterPluginModule.forRoot(),
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule
