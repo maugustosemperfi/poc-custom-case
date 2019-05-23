5e638de126aa9be14413105c1e1de1842647b938
maugustosemperfi <augustoprofemp@gmail.com>
2019-05-22 22:16:11 -0300

    [FEATURE] Hiding web layout and showing mobile

--- src/app/modules/case-container/case-container.module.ts
+++ src/app/modules/case-container/case-container.module.ts
@@ -2,13 +2,14 @@ import { DragDropModule } from '@angular/cdk/drag-drop';
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FlexLayoutModule } from '@angular/flex-layout';
+import { MaterialModule } from 'src/app/material.module';
 import { CaseEditorModule } from '../case-editor/case-editor.module';
 import { CaseTextModule } from '../case-text/case-text.module';
 import { CaseContainerComponent } from './case-container.component';
 
 @NgModule({
   declarations: [CaseContainerComponent],
-  imports: [CommonModule, FlexLayoutModule, CaseTextModule, CaseEditorModule, DragDropModule],
+  imports: [CommonModule, FlexLayoutModule, MaterialModule, CaseTextModule, CaseEditorModule, DragDropModule],
   exports: [CaseContainerComponent]
 })
 export class CaseContainerModule {}
